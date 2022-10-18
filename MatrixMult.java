a32b4c7135acccc484977268125b19766a27d7fa
phillip-che <pche@cpp.edu>
2022-10-18 01:46:58 -0700

    Implemented Divide and Conquer Algorithm

--- MatrixMult.java
+++ MatrixMult.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class MatrixMult {
 
-    private static long time = 0;
     public static void main(String args[]) {
         // int[][] a = {{3,-1,2},{0,1,-2},{1,0,2}};
         // int[][] b = {{1,0,-1},{2,0,3},{-1,4,1}};
@@ -15,13 +14,22 @@ public class MatrixMult {
         int[][] b = generateMatrix(size);
         System.out.println("\nMatrix A = \n" + Arrays.deepToString(a));
         System.out.println("\nMatrix B = \n" + Arrays.deepToString(b));
+
+        long startTime = System.nanoTime();
         System.out.println("\nClassical: \n" + Arrays.deepToString(classical(size, a, b)));
+        long endTime = System.nanoTime();
+        long time = endTime - startTime;
+        System.out.println("Execution Time: " + (time) + "\n");
+
+        startTime = System.nanoTime();
+        System.out.println("\nDivide and Conquer: \n" + Arrays.deepToString(divideAndConquer(a, b, 0, 0, 0, 0, a.length)));
+        endTime = System.nanoTime();
+        time = endTime - startTime;
         System.out.println("Execution Time: " + (time) + "\n");
     }
 
     public static int[][] classical(int n, int[][] a, int[][] b) {
         int[][] res = new int[n][n];
-        long startTime = System.nanoTime();
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
                 int sum = 0;
@@ -31,29 +39,30 @@ public class MatrixMult {
                 res[i][j] = sum;
             }
         }
-        long endTime = System.nanoTime();
-        time = endTime - startTime;
         return res;
     }
 
-    public static int[][] divideAndConquer(int n, int[][] a, int[][] b) {
+    public static int[][] divideAndConquer(int[][] a, int[][] b, int rowA, int colA, int rowB, int colB, int n) {
         int[][] res = new int[n][n];
         if(n == 1) {
-            res[0][0] = a[0][0]*b[0][0];
+            res[0][0] = a[rowA][colA]*b[rowB][colB];
             return res;
         }
 
-        int size = n/2;
+        n = n/2;
 
-        addMatrix(divideAndConquer(size, a, b), b, res, size);
+        addMatrix(divideAndConquer(a, b, rowA, colA, rowB, colB, n), divideAndConquer(a, b, rowA, colA+n, rowB+n, colB, n), res, 0, 0);
+        addMatrix(divideAndConquer(a, b, rowA, colA, rowB, colB+n, n), divideAndConquer(a, b, rowA, colA+n, rowB+n, colB+n, n), res, 0, n);
+        addMatrix(divideAndConquer(a, b, rowA+n, colA, rowB, colB, n), divideAndConquer(a, b, rowA+n, colA+n, rowB+n, colB, n), res, n, 0);
+        addMatrix(divideAndConquer(a, b, rowA+n, colA, rowB, colB+n, n), divideAndConquer(a, b, rowA+n, colA+n, rowB+n, colB+n, n), res, n, n);
 
         return res;
     }
 
-    public static void addMatrix(int[][] a, int[][] b, int[][] c, int size, int rowC, int colC) {
-        for(int i = 0; i < size; i++) {
-            for(int j = 0; j < size; j++) {
-                c[i+rowC][j+colC] = a[i][j] + b[i][j];
+    public static void addMatrix(int[][] a, int[][] b, int[][] c, int row, int col) {
+        for(int i = 0; i < a.length; i++) {
+            for(int j = 0; j < a.length; j++) {
+                c[i+row][j+col] = a[i][j] + b[i][j];
             }
         }
     } 
